0f45be367c836c8bdbba2bf6272b075f092ef620
dope <mdc@kakao.com>
2025-07-29 17:55:26 +0900

    feat: github url

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -182,7 +182,7 @@ const Projects = () => {
           className="text-center mt-12"
         >
           <motion.a
-            href="https://github.com"
+            href="https://github.com/santarosalia"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ scale: 1.05 }}
